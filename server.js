41f2e5b93b9bec1999cb2bd0334809274199eb5c
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-07 18:38:51 +0700

    Routing dan Verb request pada Hapi.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     host: 'localhost',
     port: 5000,
   })
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
